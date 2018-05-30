295cdb8a7a658335b563c496087d26bc76e04460
Chance Eakin <chance.eakin@gmail.com>
2018-05-29 21:15:54 -0500

    removed client

--- client/src/index.js
+++
@@ -1,9 +0,0 @@
-// @flow
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './containers/App'
-import registerServiceWorker from './registerServiceWorker'
-
-ReactDOM.render(<App />, document.getElementById('root'))
-registerServiceWorker()
--- emma-app/src/store.js
+++ emma-app/src/store.js
@@ -1,17 +1,13 @@
-import {
-  createStore,
-  applyMiddleware
-} from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
-import { middleware } from './utils/redux'
 import type { Store } from './types/Store'
 
 import reducers from './reducers' //Import the reducer
 
-const middlewares = [
-  thunk,
-  middleware
-]
+const middlewares = [thunk]
 
 // Connect our store to the reducers
-export const store: Store =  createStore(reducers, applyMiddleware(...middlewares))
+export const store: Store = createStore(
+  reducers,
+  applyMiddleware(...middlewares)
+)
